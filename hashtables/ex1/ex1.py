896bd81c91572aea9f04fbf1de18dd75fd1f0951
Sharanjit Sandhu <sharanjit.sandhuu@gmail.com>
2019-08-09 12:43:53 -0700

    Finished first hash table

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,17 +1,20 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
+#  Hint:  You may not need all of these. Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve,)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
+
+    for i in range(length):
+        element = limit - weights[i]
+        answer = hash_table_retrieve(ht, element)
+        if answer:
+            return (answer, i)
 
     return None
 
